bb1c5c36de05ada8c0fa47477789b4b164ebe2bf
Johnathan Nicholas <jtnicholas2014@gmail.com>
2020-10-07 16:56:00 +0100

    Added .gitignore and added Student class fields

---
+++ src/main/java/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+    private String firstName;
+    private String lastName;
+    //DOB - Need Joda time
+    private int ID;
+    private String username;
+
+    public Student(String firstName, String lastName) {
+
+    }
+}
